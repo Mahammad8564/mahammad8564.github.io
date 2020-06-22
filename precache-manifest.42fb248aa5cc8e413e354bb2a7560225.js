93c267375b5e040fc083164fe37790f1ecb2ba11
Mahammad <mahammad@sixberries.com>
2020-06-22 19:09:50 +0530

    init

---
+++ precache-manifest.42fb248aa5cc8e413e354bb2a7560225.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9fa5f9af7f4b81ab676",
+    "url": "/css/app.47c2a956.css"
+  },
+  {
+    "revision": "6506ffb469997b9b212b",
+    "url": "/css/auth.0c6bca99.css"
+  },
+  {
+    "revision": "eb3777261a651391e1d8",
+    "url": "/css/auth~chess~hanoi~ludo.4342f808.css"
+  },
+  {
+    "revision": "c8a4ac9787e0ee4e4b9a",
+    "url": "/css/chess.e789c2ba.css"
+  },
+  {
+    "revision": "0b54e48e548b562e8597",
+    "url": "/css/chess~hanoi~ludo.9df90a11.css"
+  },
+  {
+    "revision": "d5b883c9c1de6ea4ca0c",
+    "url": "/css/chunk-vendors.8d1405ad.css"
+  },
+  {
+    "revision": "e8417c72f984c1d70ae1",
+    "url": "/css/hanoi.5d3ec6d0.css"
+  },
+  {
+    "revision": "c44a66290e28d452aa6f",
+    "url": "/css/ludo.9b714b83.css"
+  },
+  {
+    "revision": "3f733cc1a24766adf0f0369acf086094",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9fa5f9af7f4b81ab676",
+    "url": "/js/app.afb9b703.js"
+  },
+  {
+    "revision": "6506ffb469997b9b212b",
+    "url": "/js/auth.d2e04c9e.js"
+  },
+  {
+    "revision": "eb3777261a651391e1d8",
+    "url": "/js/auth~chess~hanoi~ludo.f0041a56.js"
+  },
+  {
+    "revision": "c8a4ac9787e0ee4e4b9a",
+    "url": "/js/chess.aa3658f1.js"
+  },
+  {
+    "revision": "0b54e48e548b562e8597",
+    "url": "/js/chess~hanoi~ludo.c5e47e0e.js"
+  },
+  {
+    "revision": "d5b883c9c1de6ea4ca0c",
+    "url": "/js/chunk-vendors.72edb10a.js"
+  },
+  {
+    "revision": "e8417c72f984c1d70ae1",
+    "url": "/js/hanoi.a1514248.js"
+  },
+  {
+    "revision": "c44a66290e28d452aa6f",
+    "url": "/js/ludo.17cb7b10.js"
+  },
+  {
+    "revision": "aeafab77985d69ea5ca0fd021be29cb5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
